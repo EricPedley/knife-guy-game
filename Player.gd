70d908990e122c7c69b87b73dc3524e197e7a59d
ericpedley@gmail.com <ericpedley@gmail.com>
2020-08-18 18:02:28 -0700

    added skybox and improved collisions

--- Player.gd
+++ Player.gd
@@ -19,12 +19,7 @@ func _process(delta):
 	if(Input.is_action_pressed("exit")):
 		get_tree().quit()
 	if(Input.is_action_just_pressed("rclick")):
-		var aimDir 
-		if($RayCast.is_colliding()):
-			var destination = $RayCast.get_collision_point()
-			aimDir = $WeaponPoint.global_transform.origin.direction_to(destination)
-		else:
-			aimDir = Vector3(0,0,-1).rotated(Vector3(0,1,0),rotation.y)
+		var aimDir = $WeaponPoint.global_transform.origin.direction_to($RayCast.get_collision_point())
 		get_node("../Dagger").shoot(aimDir)
 	
 func _physics_process(delta):
--- Weapon.gd
+++ Weapon.gd
@@ -9,6 +9,7 @@ onready var player = get_node("../Player")
 
 var velocity=Vector3.ZERO
 var shooting=false
+var returning=false
 var parent = null
 func set_parent(newParent):
 	parent=newParent
@@ -19,26 +20,33 @@ func _ready():
 	
 func _process(delta):
 	if(Input.is_action_just_pressed("lclick")):
-		shoot(global_transform.origin.direction_to(player.global_transform.origin))
+		shoot(global_transform.origin.direction_to(player.WeaponPoint.global_transform.origin))
+		returning=true
 
 func _physics_process(delta):
 	if shooting:
-		if $RayCast.is_colliding():#when the knife hits something
+		if returning and global_transform.origin.distance_to(player.global_transform.origin)<3:
+			parent=player
 			shooting=false
-			velocity=Vector3()
-			var collider = $RayCast.get_collider()
-			if collider.is_in_group("enemies"):
-				parent=collider
-				parent.die()
-				parent.WeaponPoint.global_transform = global_transform
-			elif collider==player:
-				parent=collider
-		else:
-			move_and_collide(velocity*delta) 
+			returning=false
+		var collision = move_and_collide(velocity*delta)
+		if collision==null:
+			return
+		var collider = collision.collider
+		shooting=false
+		returning=false
+		velocity=Vector3()
+		if collider.is_in_group("enemies"):
+			$CollisionShape.set_disabled(true)
+			parent=collider
+			parent.die()
+			parent.WeaponPoint.global_transform = global_transform
+			
 	elif parent!=null and (parent==player or parent.is_in_group("enemies")):
 		global_transform = parent.WeaponPoint.global_transform
 		#apply_central_impulse((plPos-myPos).normalized()*plPos.distance_to(myPos))
 func shoot(vector):
+	$CollisionShape.set_disabled(false)
 	shooting=true
 	velocity = vector.normalized()*50
 	look_at(velocity,Vector3(0,1,0))
